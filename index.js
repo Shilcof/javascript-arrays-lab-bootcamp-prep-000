7cf6eeb84bb7740d84c4fd991f7a219ef2fd14e0
Robert Shilcof <robert.shilcof@gmail.com>
2020-09-08 10:46:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function destructivelyAppendKitten(array, element) {
   return array
 }
 
-function destructivelyAppendKitten(array, element) {
+function appendKitten(array, element) {
   array = [...array, element];
   return array
 }
